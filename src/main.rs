ebc2a1bbde8cbe7d8a7fd845b0ce3fbec2e6a83e
xudesheng <xudesheng@gmail.com>
2019-08-09 16:18:54 -0400

    recover since signle-hook doesn't support windows.

--- src/main.rs
+++ src/main.rs
@@ -22,41 +22,11 @@ use clap::{Arg, App}; //, SubCommand
 use std::process;
 use influx_db_client::{Point, Points};
 use std::error::Error;
-use std::{thread};
+use std::{thread, time};
 
-// extern crate ctrlc;
-// use std::sync::atomic::{AtomicBool, Ordering};
-// use std::sync::Arc;
-#[macro_use]
-extern crate crossbeam_channel;
-extern crate signal_hook;
-use std::io;
-use std::time::{Duration, Instant};
-use crossbeam_channel::{tick, unbounded, Receiver};
-use signal_hook::SIGINT;
-use signal_hook::iterator::Signals;
-
-// Creates a channel that gets a message every time `SIGINT` is signalled.
-fn sigint_notifier() -> io::Result<Receiver<()>> {
-    let (s, r) = unbounded();
-    let signals = Signals::new(&[SIGINT])?;
-
-    thread::spawn(move || {
-        for _ in signals.forever() {
-            if s.send(()).is_err() {
-                break;
-            }
-        }
-    });
-
-    Ok(r)
-}
-
-// Prints the elapsed time.
-fn show(dur: Duration) {
-    info!("Elapsed: {}.{:03} sec", dur.as_secs(), dur.subsec_nanos() / 1_000_000);
-}
-
+extern crate ctrlc;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 
 fn main() ->Result<(),Box<dyn Error>>{
     //let env = pretty_env_logger::Env::new().filter("TSAMPLE_LOG");
@@ -73,7 +43,7 @@ fn main() ->Result<(),Box<dyn Error>>{
     //     })
     //     .init();
 
-    
+    info!("TSAMPLE Started.");
 
     let matches = App::new("Thingworx Sampler")
             .version("0.0.1")
@@ -109,7 +79,7 @@ fn main() ->Result<(),Box<dyn Error>>{
     if matches.is_present("export") {
         match ThingworxTestConfig::export_sample(config_file) {
             Ok(()) => {
-                info!("Sample configuration file has been exported to:{}", config_file);
+                debug!("Sample configuration file has been exported to:{}", config_file);
                 process::exit(0);
             },
             Err(e) => {
@@ -122,8 +92,6 @@ fn main() ->Result<(),Box<dyn Error>>{
             },
         }
     }
-
-    info!("TSAMPLE Started.");
     //tsample::ThingworxTestConfig::export_sample(config_file)?;
     let testconfig = match ThingworxTestConfig::from_tomefile(config_file) {
         Ok(conf) => conf,
@@ -137,77 +105,67 @@ fn main() ->Result<(),Box<dyn Error>>{
         Some(minutes) => minutes*60*1000,
         None => 1*60*1000,
     };
-    let start = Instant::now();
-    let update = tick(Duration::from_millis(sleep-20));
-    let ctrl_c = sigint_notifier().unwrap();
+    let sleep_duration = time::Duration::from_millis(sleep);
 
-    // let sleep_duration = time::Duration::from_millis(sleep);
+    let running = Arc::new(AtomicBool::new(true));
+    let sleeping = Arc::new(AtomicBool::new(false));
 
-    // let running = Arc::new(AtomicBool::new(true));
-    // let sleeping = Arc::new(AtomicBool::new(false));
+    let r = running.clone();
+    let s = sleeping.clone();
 
-    // let r = running.clone();
-    // let s = sleeping.clone();
-
-    // ctrlc::set_handler(move || {
-    //     println!("Received Ctrl-C from console.");
-    //     r.store(false, Ordering::SeqCst);
-    //     if sleeping.load(Ordering::SeqCst) {
-    //         println!("Quit from sleeping...");
-    //         process::exit(0);
-    //     }
-    // }).expect("Error setting Ctrl-C handler");
+    ctrlc::set_handler(move || {
+        println!("Received Ctrl-C from console.");
+        r.store(false, Ordering::SeqCst);
+        if sleeping.load(Ordering::SeqCst) {
+            println!("Quit from sleeping...");
+            process::exit(0);
+        }
+    }).expect("Error setting Ctrl-C handler");
 
     let servers = match testconfig.thingworx_servers {
         Some(servers) => servers,
         None => {vec![]},
     };
 
-    //while running.load(Ordering::SeqCst){
-    loop{
-        select!{
-            recv(update) -> _ => {
-                show(start.elapsed());
-                info!("start repeated sampling...");
-                let point = sampling::sampling_repeat(&testconfig.testmachine.testid, &testconfig.testmachine.repeat_sampling);
-                //debug!("sampling_repeat: {:?}\n", point);
+    while running.load(Ordering::SeqCst){
+        info!("start repeated sampling...");
+        let point = sampling::sampling_repeat(&testconfig.testmachine.testid, &testconfig.testmachine.repeat_sampling);
+        //debug!("sampling_repeat: {:?}\n", point);
 
-                let mut total_points:Vec<Point> = Vec::new();
-                match point {
-                    Ok(p) => total_points.push(p),
-                    Err(e) =>{error!("Error:{}", e);},
-                }
-
-                
-                for server in &servers {
-                    let points = sampling::sampling_thingworx(server);
-                    //debug!("thingworx_servers:{:?}\n", points);
-                    match points {
-                        Ok(mut ps) => total_points.append(&mut ps),
-                        Err(e) => {info!("Error:{}", e);},
-                    }
-                }
-            
-                
-                debug!("Total Points:{}", total_points.len());
-
-                let myclient = MyInfluxClient::new(&testconfig.test_data_target);
-
-                match myclient.write_points(Points::create_new(total_points)) {
-                    Ok(()) => {},
-                    Err(e) => {error!("Error: {}", e);},
-                }
-                info!("finished repeated sampling...");
-            }
-            recv(ctrl_c) -> _ => {
-                println!();
-                println!("Goodbye!");
-                show(start.elapsed());
-                break;
+        let mut total_points:Vec<Point> = Vec::new();
+        match point {
+            Ok(p) => total_points.push(p),
+            Err(e) =>{error!("Error:{}", e);},
+        }
+        
+        for server in &servers {
+            let points = sampling::sampling_thingworx(server);
+            //debug!("thingworx_servers:{:?}\n", points);
+            match points {
+                Ok(mut ps) => total_points.append(&mut ps),
+                Err(e) => {info!("Error:{}", e);},
             }
         }
+        
+        debug!("Total Points:{}", total_points.len());
+
+        let myclient = MyInfluxClient::new(&testconfig.test_data_target);
+
+        match myclient.write_points(Points::create_new(total_points)) {
+            Ok(()) => {},
+            Err(e) => {error!("Error: {}", e);},
+        }
+
+        if !running.load(Ordering::SeqCst){break;}
+
+        info!("Sleeping...");
+        s.store(true, Ordering::SeqCst);
+        thread::sleep(sleep_duration);
+        s.store(false, Ordering::SeqCst);
     }
     
     info!("we have done.");
+    
+
     Ok(())
 }
